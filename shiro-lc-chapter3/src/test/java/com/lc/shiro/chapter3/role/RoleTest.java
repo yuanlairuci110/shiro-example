cac1f1f296eb4f1bfc005b4b9041dab32ed4c80d
lichaoi <lichaoi@yonyou.com>
2017-05-26 14:46:39 +0800

    判断是否拥有角色

---
+++ shiro-lc-chapter3/src/test/java/com/lc/shiro/chapter3/role/RoleTest.java
@@ -0,0 +1,27 @@
+package com.lc.shiro.chapter3.role;
+
+import java.util.Arrays;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.lc.shiro.chapter3.utils.BaseTest;
+
+
+
+public class RoleTest extends BaseTest{
+	@SuppressWarnings("deprecation")
+	@Test
+	public void testHasRole(){
+		login("classpath:shiro-role.ini", "zhang", "123");
+		//判断用友角色 ： role1
+		Assert.assertTrue(subject().hasRole("role1"));
+		//判断拥有角色：role1 and role2
+		Assert.assertTrue(subject().hasAllRoles(Arrays.asList("role1", "role2")));
+		//判断拥有角色：role1 and role2 and !role3
+		boolean[] result = subject().hasRoles(Arrays.asList("role1", "role2", "role3"));
+		Assert.assertEquals(true, result[0]);
+		Assert.assertEquals(true, result[1]);
+		Assert.assertEquals(false, result[2]);
+	}
+}
