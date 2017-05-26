0bb2cefc215a95120043b76a5a51627017ffd4cd
lichaoi <lichaoi@yonyou.com>
2017-05-26 15:47:41 +0800

    拥有权限

--- shiro-lc-chapter3/src/test/java/com/lc/shiro/chapter3/permission/PermissionTest.java
+++ shiro-lc-chapter3/src/test/java/com/lc/shiro/chapter3/permission/PermissionTest.java
@@ -1,5 +1,6 @@
 package com.lc.shiro.chapter3.permission;
 
+import org.apache.shiro.authz.UnauthorizedException;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -16,5 +17,32 @@ public class PermissionTest extends BaseTest{
 		Assert.assertTrue(subject().isPermittedAll("user:update", "user:delete"));
 		//判断拥有权限：user:update and user:delete
 		Assert.assertTrue(subject().isPermittedAll("user:update", "user:delete"));
+	}
+
+	@Test(expected = UnauthorizedException.class)
+	public void testCheckPermission() {
+		login("classpath:shiro-permission.ini", "zhang", "123");
+		//断言拥有权限：user:create
+		subject().checkPermission("user:create");
+		//断言拥有权限：user:delete and user:update
+		subject().checkPermissions("user:delete", "user:update");
+		//断言拥有权限：user:view 失败抛出异常
+		subject().checkPermissions("user:view");
+	}
+
+	@Test
+	public void testCheckPermissionException(){
+		login("classpath:shiro-permission.ini", "zhang", "123");
+		//断言拥有权限：user:create
+		subject().checkPermission("user:create");
+		//断言拥有权限：user:delete and user:update
+		subject().checkPermissions("user:delete", "user:update");
+		//断言拥有权限：user:view 失败抛出异常
+		try {
+			subject().checkPermissions("user:view");
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		
 	}
 }
